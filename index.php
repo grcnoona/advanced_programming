296b5c4369821e77e30ee7d41bec21c9260fa649
grcnoona <grasyamallen@gmail.com>
2020-11-05 18:20:56 +0800

    Added index.php for adding friend and posting a status

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?php
+    class User{
 
-  echo "Hello World";
+      public $name;
+      public $username;
+      public $email;
+
+      function addFriend($name){
+        echo "$name" . "added a friend. <br>";
+      }
+      function postStatus($name){
+        echo "$name" . "posted a status. <br>";
+      }
+    }
+      $new = new User()
+      echo $new->addFriend('Mary Grace Ragpala');
+      echo "<br>";
+      echo $new->postStatus('Mary Grace Ragpala');
+
  ?>
